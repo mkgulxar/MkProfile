30c8174555d8fc2df012b39c3c9a6a2394f4a9b2
mkgulxar <mkgulzar@gmail.com>
2023-01-29 00:47:37 +0500

    update

---
+++ fh/src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+const Home = () => {
+    return (
+        <>
+        <h1>It is a world class group</h1>
+        <h2>which offers experience</h2>
+        <p>equipment, and overall know-how of what is needed to build a business from scratch. with years of experience, required facilities and resources under one roof fh provides one stop solution to all its clients which not only saves time and energy but the creative output that is unlikely to be found elsewhere.</p>
+        </>
+    )
+}
+export default Home
---
+++ fh/src/pages/Services.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Services = () => {
+    return (
+        <>
+        <h1>servics Page</h1>
+        </>
+    )
+}
+export default Services
