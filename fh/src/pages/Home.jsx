3fe4c3a1f92a97d3739106a2a34ae721edc30540
mkgulxar <mkgulzar@gmail.com>
2023-01-29 01:18:11 +0500

    mks

--- fh/src/pages/Home.jsx
+++ fh/src/pages/Home.jsx
@@ -4,9 +4,12 @@ import React from "react";
 const Home = () => {
     return (
         <>
-        <h1>It is a world class group</h1>
-        <h2>which offers experience</h2>
+        <div style={{margin:'10%'}}>
+
+        <h3>It is a world class group</h3>
+        <h4>which offers experience</h4>
         <p>equipment, and overall know-how of what is needed to build a business from scratch. with years of experience, required facilities and resources under one roof fh provides one stop solution to all its clients which not only saves time and energy but the creative output that is unlikely to be found elsewhere.</p>
+        </div>
         </>
     )
 }
