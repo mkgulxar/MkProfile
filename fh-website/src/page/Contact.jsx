8b3c0e812962ca65642c33c4b20f45a46a993bc9
mkgulxar <mkgulzar@gmail.com>
2023-01-31 16:37:50 +0500

    mk

--- fh-website/src/page/Contact.jsx
+++ fh-website/src/page/Contact.jsx
@@ -3,7 +3,12 @@ import React from 'react'
 const Contact = () => {
   return (
     <>
-    <h1>Contact</h1>
+  <div style={{ textAlign:'center' ,display:'flex', flexDirection:'column',}}>
+  <h1>CONTACT</h1>
+  <h2>unique and recognizable</h2>
+  <p >Identity that align with your business needs and are
+recognizable as “yours”. Essentially, these graphics will contains all the feature elements that are unique and recognizable for your business</p>
+  </div>
     </>
   )
 }
--- fh-website/src/page/Home.jsx
+++ fh-website/src/page/Home.jsx
@@ -8,14 +8,14 @@ const Home = () => {
   <div style={{height:'500px', display:'flex', margin:'auto', alignItems:'center',justifyContent:'center' }}>
 
 
-  <div style={{ textAlign:'right' ,marginTop:'-15%', width:'50%'}}>
+  <div style={{ textAlign:'right' ,marginTop:'-15pt', width:'50%'}}>
   <h1>BRANDING</h1>
   <h2>unique and recognizable</h2>
   <p >Identity that align with your business needs and are
 recognizable as “yours”. Essentially, these graphics will contains all the feature elements that are unique and recognizable for your business</p>
   </div>
 
-  <div style={{width:'40%'}}>
+  <div style={{width:'30%'}}>
 <img src={ Paint} alt="FH Group" style={{height:'500px'}} />
 </div>
   </div>
