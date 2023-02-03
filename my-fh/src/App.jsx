b817da35d9cbfd1375729b3ba529940949ab22ec
mkgulxar <mkgulzar@gmail.com>
2023-02-03 12:01:18 +0500

    update

--- my-fh/src/App.jsx
+++ my-fh/src/App.jsx
@@ -1,10 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
 
+import ImageComponent from './pages/imageTest/ImageComponent';
+const pic= "https://i.pinimg.com/736x/c5/61/ab/c561ab3d23014eb1499065962a8fa449.jpg"
+
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+   {/* <img src='https://i.pinimg.com/736x/c5/61/ab/c561ab3d23014eb1499065962a8fa449.jpg'/> */}
+      <ImageComponent src={pic} alt="image description" />
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
---
+++ my-fh/src/pages/imageTest/ImageComponent.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const ImageComponent = (props) => {
+  return <img src={props.src} alt={props.alt} />;
+};
+
+export default ImageComponent;
