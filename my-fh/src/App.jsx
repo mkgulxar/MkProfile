89cc2c555325203fe8cad52db6cb7b4128322e3a
Alihaider7707 <alihd8330@gmail.com>
2023-02-03 12:23:19 +0500

    update

--- my-fh/src/App.jsx
+++ my-fh/src/App.jsx
@@ -1,30 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
 
-import ImageComponent from './pages/imageTest/ImageComponent';
-const pic= "https://i.pinimg.com/736x/c5/61/ab/c561ab3d23014eb1499065962a8fa449.jpg"
 
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-   {/* <img src='https://i.pinimg.com/736x/c5/61/ab/c561ab3d23014eb1499065962a8fa449.jpg'/> */}
-      <ImageComponent src={pic} alt="image description" />
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/About" component={About} />
+        <Route exact path="/Contact" component={Contact} />
+        <Route exact path="/Socialmesia" component={Socialmesia} />
+        <Route exact path="/Service" component={Service} />
+      
+       
+
+        <Redirect to="/" />
+      </Switch>
+    </>
   );
 }
 
