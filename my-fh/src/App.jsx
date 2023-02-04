9430d36fa46f89701099d39e71262436285e838c
Alihaider7707 <alihd8330@gmail.com>
2023-02-04 18:04:10 +0500

    mk

--- my-fh/src/App.jsx
+++ my-fh/src/App.jsx
@@ -1,33 +1,35 @@
-import './App.css';
-import Home from './pages/Home'
-import About from './pages/About'
-import Contact from './pages/Contact'
-import Socialmesia from './pages/Socialmesia'
-import Service from './pages/Service'
-import Header from './pages/Header'
-import Footer from './pages/Footer'
-import Team from './pages/Team';
+import "./App.css";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Socialmesia from "./pages/Socialmesia";
+import Service from "./pages/Service";
+import Header from "./pages/Header";
+import Footer from "./pages/Footer";
+import Team from "./pages/Team";
+import Text from "./pages/Text";
+import image from "./pages/image";
 
-
-
-import { Switch, Redirect, Route } from 'react-router-dom';
+import { Switch, Redirect, Route } from "react-router-dom";
 
 function App() {
   return (
     <>
       <Header />
-   
+
       <Switch>
-        <Route exact path="/" component={Home} /> 
+        <Route exact path="/" component={Home} />
         <Route exact path="/About" component={About} />
         <Route exact path="/Contact" component={Contact} />
         <Route exact path="/Socialmesia" component={Socialmesia} />
         <Route exact path="/Service" component={Service} />
         <Route exact path="/Team" component={Team} />
+        <Route exact path="/Text" component={Text} />
+        <Route exact path="/image" component={image} />
 
         <Redirect to="/" />
       </Switch>
-      <Footer/>
+      <Footer />
     </>
   );
 }
