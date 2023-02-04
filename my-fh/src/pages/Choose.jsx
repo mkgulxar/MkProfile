c3fafcb6fda87d8439e979f2b76c2845e82f72ff
Alihaider7707 <alihd8330@gmail.com>
2023-02-04 18:19:55 +0500

    mk

--- my-fh/src/pages/Choose.jsx
+++ my-fh/src/pages/Choose.jsx
@@ -25,17 +25,23 @@ const Choose = () => {
       </div>
       <div className="team mask-2">
         <div className="team-work2 teamw">
+        <Link to="/image" target="_blank" rel="noreferrer">
             <img src="https://i.pinimg.com/564x/47/54/2f/47542fa57172ccfca271aec7d1da6590.jpg" alt="" />
+            </Link>
         </div>
       </div>
       <div className="team mask-3">
         <div className="team-work3 teamw">
+        <Link to="/image" target="_blank" rel="noreferrer">
             <img src="https://i.pinimg.com/564x/19/56/98/1956986a8baae5037c266e63a927d478.jpg" alt="" />
+            </Link>
         </div>
       </div>
       <div className="team mask-4">
         <div className="team-work4 teamw">
+        <Link to="/image" target="_blank" rel="noreferrer">
             <img src="https://i.pinimg.com/736x/c1/1e/4a/c11e4ae2c340e00adb4aa7bfed572a82.jpg" alt="" />
+            </Link>
         </div>
       </div>
       </div>
