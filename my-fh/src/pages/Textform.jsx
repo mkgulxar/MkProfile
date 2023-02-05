896bf8154625f7c7c239d6c7a85c432f55cef660
Alihaider7707 <alihd8330@gmail.com>
2023-02-05 16:59:12 +0500

    up

--- my-fh/src/pages/Textform.jsx
+++ my-fh/src/pages/Textform.jsx
@@ -1,24 +1,42 @@
 import React, { useState } from "react";
 
 export default function Textform(props) {
-  const handleUpClick = () => {
-    // console.log("Uppercase was clicked" + text)
-    let newText = text.toUpperCase();
-    setText(newText);
-  };
+  //   const handleUpClick = () => {
+  //     console.log("Uppercase was clicked" + text)
+  //     let newText = text.toUpperCase();
+  //     setText(newText);
+  //   };
+  //   const handleOnChange = (event) => {
+  //     console.log("On change")
+  //     setText(event.target.value);
+  //   };
+  //   const [text, setText] = useState("Enter text here");
 
-  const handleOnChange = (event) => {
-    // console.log("On change")
-    setText(event.target.value);
-  };
 
-  const [text, setText] = useState("Enter text here");
+  
+  const [data, setData] = useState(null);  
+  const [print, setPrint] = useState(false);
+  function getData(val) {
+    setData(val.target.value);
+    setPrint(false)
+    console.warn(val.target.value);
+  }
+
   return (
-    <div>
-      <h1>{props.heading} </h1>
+    // <div>
+    //   <h1>{props.heading} </h1>
 
-      <textarea value={text} onChange={handleOnChange}></textarea>
-      <button onClick={handleUpClick}>uppercace</button>
-    </div>
+    //   <textarea value={text} onChange={handleOnChange}></textarea>
+    //   <button onClick={handleUpClick}>uppercace</button>
+    // </div>
+    <>
+      {
+        print?
+        <h1>{data}</h1>
+        :null
+      }
+      <input type="text" on onChange={getData} />
+      <button onClick={()=>setPrint(true)}>Print Value</button>
+    </>
   );
 }
