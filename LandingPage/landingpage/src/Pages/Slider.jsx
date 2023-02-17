8ece824f3c366f6fca636e8a67497d8dd1906c78
Alihaider7707 <alihd8330@gmail.com>
2023-02-17 18:28:23 +0500

    ali haider

--- LandingPage/landingpage/src/Pages/Slider.jsx
+++ LandingPage/landingpage/src/Pages/Slider.jsx
@@ -1,14 +1,52 @@
 import React from "react";
+import "../Pages/Slider.css";
 import { GiChefToque } from "react-icons/gi";
 
 function Slider() {
   return (
     <>
-      <div className="best-slider-text">
-        <h2>Our Best Programs</h2>
-      </div>
-      <div className="box-icons">
-        <GiChefToque />
+      <div className="maxWidth">
+        <div className="best-slider-text">
+          <h2>Our Best Programs</h2>
+        </div>
+        <div className="box-flex">
+          <div className="box-background">
+            <div className="box-icons">
+              <GiChefToque />
+            </div>
+            <div className="box-text">
+              <h2>Healthy Recipe</h2>
+              <p>Healthier Recipes, from food & nutriion experts</p>
+            </div>
+          </div>
+          <div className="box-background">
+            <div className="box-icons">
+              <GiChefToque />
+            </div>
+            <div className="box-text">
+              <h2>Healthy Recipe</h2>
+              <p>Healthier Recipes, from food & nutriion experts</p> 
+            </div>
+          </div>
+          <div className="box-background">
+            <div className="box-icons">
+              <GiChefToque />
+            </div>
+            <div className="box-text">
+              <h2>Healthy Recipe</h2>
+              <p>Healthier Recipes, from food & nutriion experts</p>
+            </div>
+          </div>
+          <div className="box-background">
+            <div className="box-icons">
+              <GiChefToque />
+            </div>
+            <div className="box-text">
+              <h2>Healthy Recipe</h2>
+              <p>Healthier Recipes, from food & nutriion experts</p>
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
